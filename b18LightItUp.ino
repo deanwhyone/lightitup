0f0ec8d939ac630352e3ea5c288cb7495c6c69c6
deanwhyone <35477361+deanwhyone@users.noreply.github.com>
2019-01-14 19:35:07 -0500

    on/off midi handlers set up

--- b18LightItUp.ino
+++ b18LightItUp.ino
@@ -15,17 +15,34 @@ CRGB leds[NUM_PIXELS];
   // pin 13: Serial Clock
 #define DATA_PIN 11
 #define CLOCK_PIN 13
-  
+
+// standard MIDI messages, functions called by handler
+// middle C is 60
+void OnNoteOn(byte channel, byte note, byte velocity) {
+  leds[note%125] = CRGB::SeaGreen;
+  FastLED.show();
+  delay(30);
+}
+void OnNoteOff(byte channel, byte note, byte velocity) {
+  leds[note%125] CRGB::Black;
+  FastLED.show();
+  delay(1000);
+}
+
 void setup() {
   // tell FastLED that there are NUM_PIXEL DotStar LEDS on DATA_PIN
-  FastLED.addLeds<LED_TYPE, DATA_PIN, CLOCK_PIN, COLOR_ORDER, DATA_RATE_MHZ(24)>
+  FastLED.addLeds<LED_TYPE, DATA_PIN, CLOCK_PIN, COLOR_ORDER>
     (leds, NUM_PIXELS);
 
   // default brightness
   FastLED.setBrightness(96);
+  
+  // set MIDI handlers
+  usbMIDI.setHandleNoteOn(OnNoteOn);
+  usbMIDI.setHandleNoteOff(OnNoteOff);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  usbMIDI.read();
 }
